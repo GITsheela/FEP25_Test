03c9a85f0f9b754083644f2ee78c7e8b2b54abe5
GITsheela <sheelajan12@gmail.com>
2025-02-26 10:36:57 +0530

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -3,7 +3,7 @@ package com.java.junit;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
+//string
 @RunWith(Suite.class)
 @SuiteClasses({TestOne.class,TestTwo.class})
 public class Runner {
